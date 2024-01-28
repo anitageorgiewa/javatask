753dccef0832546c7a3bae59f171744b516c2374
anita <anitageorgiewa@gmail.com>
2024-01-28 16:26:55 +0200

    Initial commit

---
+++ src/test/java/com/is/javatask/JavataskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.is.javatask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavataskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
