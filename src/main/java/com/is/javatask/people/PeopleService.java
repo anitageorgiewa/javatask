62a6047eaf3ef8368ede5a22c5138d78785bd6dd
anita <anitageorgiewa@gmail.com>
2024-01-30 10:40:49 +0200

    progress

--- src/main/java/com/is/javatask/people/PeopleService.java
+++ src/main/java/com/is/javatask/people/PeopleService.java
@@ -1,6 +1,8 @@
 package com.is.javatask.people;
 
+import com.is.javatask.people.dto.MailsDto;
 import com.is.javatask.people.dto.PeopleDto;
+import com.is.javatask.people.model.MailsEntity;
 import com.is.javatask.people.model.PeopleEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,12 +13,16 @@ import java.util.List;
 public class PeopleService {
 
     private PeopleRepository repository;
+    private MailsRepository mailsRepo;
+    private AddressesRepository addrRepo;
     private PeopleMapper peopleMapper;
 
     @Autowired
-    public PeopleService(PeopleRepository repository, PeopleMapper peopleMapper) {
+    public PeopleService(PeopleRepository repository, PeopleMapper peopleMapper,MailsRepository mailsRepo,AddressesRepository addrRepo) {
         this.repository = repository;
         this.peopleMapper = peopleMapper;
+        this.addrRepo = addrRepo;
+        this.mailsRepo = mailsRepo;
     }
 
     public List<PeopleDto> searchPeople(String search) {
@@ -28,4 +34,10 @@ public class PeopleService {
          PeopleEntity peopleEntity = peopleMapper.map(peopleDto);
          repository.save(peopleEntity);
     }
+
+    public  List<MailsDto> findAllMails(Integer peopleID){
+        return  peopleMapper.mapMails( mailsRepo.findByPeopleId(peopleID));
+    }
+
+
 }
